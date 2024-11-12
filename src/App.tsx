93c3c2aa2347c951361776b57bce0a03ea694bb0
Michael Lee <mlee@Michaels-Macbook-Pro.local>
2024-11-11 22:54:44 -0500

    fixed nav for site by moving router

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Concerts from './Home/Home';
-import Contact from './Contact/Contact';
-import image from './images/Cupid-Logo.png'
+import Contact from './Contact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<Concerts />} />
-          <Route path="contact" element={<Contact/>} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Concerts />} />
+            <Route path="/contact" element={<Contact/>} />
+          </Routes>
+        </Router>
     </div>
   );
 }
